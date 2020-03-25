4057b27c4aa87e106487f84cc9e77a3a71d3e958
Tatsada <62229137+Tatsada@users.noreply.github.com>
2020-03-26 00:08:56 +0700

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -1,6 +1,10 @@
+listSingleDigit = ["không", "một", "hai", "ba",
+                   "bốn", "năm", "sáu", "bảy", "tám", "chín"]
+listDigitGroup = ["", "nghìn", "triệu", "tỷ"]
+
+
 def splitDigitGroup(inputNum):
-    numOfDigit = len(inputNum)
-    numOfDigitGroup = numOfDigit // 3 + 1
+    numOfDigitGroup = (len(inputNum) // 3) + 1
     digitGroup = [inputNum[-3:]]
     for i in range(1, numOfDigitGroup - 1):
         digitGroup.append(inputNum[-(3 * i + 3):-3 * i])
@@ -8,47 +12,42 @@ def splitDigitGroup(inputNum):
 
 
 def readSingleGroup(inputNum):
-    listSingleDigit = ["không", "một", "hai", "ba",
-                       "bốn", "năm", "sáu", "bảy", "tám", "chín"]
-    if int(inputNum) < 99:
-        return readTenAndUnitDigit(inputNum)
-    else:
-        if inputNum[-2] == "0":
-            if inputNum[-1] == "0":
-                readTwoLastDigit = ""
-            else:
-                readTwoLastDigit = "linh " + listSingleDigit[int(inputNum[-1])]
-        else:
-            readTwoLastDigit = readTenAndUnitDigit(inputNum)
-        return listSingleDigit[int(inputNum[-3])] + " trăm " + readTwoLastDigit
+    readHundredDigit = listSingleDigit[int(inputNum[-3])] + " trăm"
 
-
-def readTenAndUnitDigit(inputNum):
-    listSingleDigit = ["không", "một", "hai", "ba",
-                       "bốn", "năm", "sáu", "bảy", "tám", "chín"]
-    if int(inputNum) <= 9:
-        return listSingleDigit[int(inputNum)]
-    else:
-        if inputNum[-2] == "1":
-            readTenDigit = "mười"
-        else:
-            readTenDigit = listSingleDigit[int(inputNum[-2])] + " mươi"
+    if inputNum[-2] == "1":
+        readTenDigit = " mười"
+    elif inputNum[-2] == "0":
         if inputNum[-1] == "0":
-            readUnitDigit = ""
-        elif inputNum[-1] == "5":
-            readUnitDigit = "lăm"
+            readTenDigit = ""
         else:
-            readUnitDigit = listSingleDigit[int(inputNum[-1])]
-    return readTenDigit + " " + readUnitDigit
+            readTenDigit = " linh"
+    else:
+        readTenDigit = " " + listSingleDigit[int(inputNum[-2])] + " mươi"
+
+    if inputNum[-1] == "0":
+        readUnitDigit = ""
+    elif inputNum[-1] == "1":
+        if inputNum[-2] == "0" or inputNum[-2] == "1":
+            readUnitDigit = " một"
+        else:
+            readUnitDigit = " mốt"
+    elif inputNum[-1] == "5":
+        if inputNum[-2] == "0":
+            readUnitDigit = " năm"
+        else:
+            readUnitDigit = " lăm"
+    else:
+        readUnitDigit = " " + listSingleDigit[int(inputNum[-1])]
+
+    return readHundredDigit + readTenDigit + readUnitDigit
 
 
 def numberToText(inputNum):
-    listDigitGroup = ["", "nghìn", "triệu", "tỷ"]
-    numberToTex = ""
+    numberToText = ""
     for i in range(len(splitDigitGroup(inputNum)) - 1, -1, -1):
-        numberToTex += (" " + readSingleGroup(splitDigitGroup(inputNum)
-                                              [i]) + " " + listDigitGroup[i])
-    return numberToTex
+        numberToText += (readSingleGroup(splitDigitGroup(inputNum)
+                                               [i]) + " " + listDigitGroup[i])
+    return numberToText
 
 
 n = input("Nhập số : ")
