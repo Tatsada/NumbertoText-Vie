c446e21044f0f878c4b5603b42aac2978cc2685a
Tatsada <62229137+Tatsada@users.noreply.github.com>
2020-03-26 16:53:18 +0700

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -19,41 +19,54 @@ def splitDigitGroup(inputNum):
     return digitGroup
 
 
-def readSingleGroupFull(inputNum):
-    readHundredDigit = listSingleDigit[int(inputNum[-3])] + " trăm"
-
-    if inputNum[-2] == "1":
-        readTenDigit = " mười"
-    elif inputNum[-2] == "0":
-        if inputNum[-1] == "0":
-            readTenDigit = ""
-        else:
-            readTenDigit = " linh"
-    else:
-        readTenDigit = " " + listSingleDigit[int(inputNum[-2])] + " mươi"
-
-    if inputNum[-1] == "0":
-        readUnitDigit = ""
-    elif inputNum[-1] == "1":
-        if inputNum[-2] == "0" or inputNum[-2] == "1":
-            readUnitDigit = " một"
-        else:
-            readUnitDigit = " mốt"
-    elif inputNum[-1] == "5":
-        if inputNum[-2] == "0":
-            readUnitDigit = " năm"
-        else:
-            readUnitDigit = " lăm"
-    else:
+def readSingleGroup(inputNum):
+	readHundredDigit = (
+	    listSingleDigit[int(inputNum[-3])] + " trăm") if len(inputNum) == 3 else ""
+    # Đọc số hàng đơn vị
+	if inputNum[-1] == "0":
+	        readUnitDigit = ""
+	elif inputNum[-1] == "1":
+		if inputNum[-2] == "0" or inputNum[-2] == "1" or len(inputNum)==1:
+			readUnitDigit = " một"
+		else:
+			readUnitDigit = " mốt"
+	elif inputNum[-1] == "5":
+		if inputNum[-2] == "0":
+			readUnitDigit = " năm"
+		else:
+			readUnitDigit = " lăm"
+	else:
         readUnitDigit = " " + listSingleDigit[int(inputNum[-1])]
-
-    return readHundredDigit + readTenDigit + readUnitDigit
-
-
-def readSingleGroupMissing(inputNum):
-    if len(inputNum) == 1:
-        return listSingleDigit[int(inputNum)]
-    else:
+    '''if len(inputNum) == 3:		# Đủ 3 chữ số
+        readHundredDigit = listSingleDigit[int(inputNum[-3])] + " trăm"
+        # Đọc số hàng chục
+        if inputNum[-2] == "1":
+            readTenDigit = " mười"
+        elif inputNum[-2] == "0":
+            if inputNum[-1] == "0":
+                readTenDigit = ""
+            else:
+                readTenDigit = " linh"
+        else:
+            readTenDigit = " " + listSingleDigit[int(inputNum[-2])] + " mươi"
+        # Đọc số hàng đơn vị
+        if inputNum[-1] == "0":
+            readUnitDigit = ""
+        elif inputNum[-1] == "1":
+            if inputNum[-2] == "0" or inputNum[-2] == "1":
+                readUnitDigit = " một"
+            else:
+                readUnitDigit = " mốt"
+        elif inputNum[-1] == "5":
+            if inputNum[-2] == "0":
+                readUnitDigit = " năm"
+            else:
+                readUnitDigit = " lăm"
+        else:
+            readUnitDigit = " " + listSingleDigit[int(inputNum[-1])]
+        return readHundredDigit + readTenDigit + readUnitDigit
+    elif len(inputNum) == 2:
+        readHundredDigit = ""
         if inputNum[-2] == "1":
             readTenDigit = "mười"
         else:
@@ -70,21 +83,18 @@ def readSingleGroupMissing(inputNum):
             readUnitDigit = " lăm"
         else:
             readUnitDigit = " " + listSingleDigit[int(inputNum[-1])]
-
-        return readTenDigit + readUnitDigit
+    else:
+        readHundredDigit = ""
+        readTenDigit = ""
+        readUnitDigit = listSingleDigit[int(inputNum)]
+    return readHundredDigit + readTenDigit + readUnitDigit'''
 
 
 def numberToText(inputNum):
     listGroupToText = []
     for i in range(len(splitDigitGroup(inputNum)) - 1, -1, -1):
-        if len(splitDigitGroup(inputNum)[i]) < 3:
-            listGroupToText.append(readSingleGroupMissing(
-                splitDigitGroup(inputNum)[i]))
-            listGroupToText.append(listDigitGroup[i])
-        else:
-            listGroupToText.append(readSingleGroupFull(
-                splitDigitGroup(inputNum)[i]))
-            listGroupToText.append(listDigitGroup[i])
+        listGroupToText.append(readSingleGroup(splitDigitGroup(inputNum)[i]))
+        listGroupToText.append(listDigitGroup[i])
     return " ".join(listGroupToText)
 
 
