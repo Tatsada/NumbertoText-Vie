b700fba9ee860f5332b17f98b7b460c09650ddb2
Tatsada <62229137+Tatsada@users.noreply.github.com>
2020-03-26 11:25:30 +0700

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -1,17 +1,25 @@
+import math
 listSingleDigit = ["không", "một", "hai", "ba",
                    "bốn", "năm", "sáu", "bảy", "tám", "chín"]
 listDigitGroup = ["", "nghìn", "triệu", "tỷ"]
 
 
 def splitDigitGroup(inputNum):
-    numOfDigitGroup = (len(inputNum) // 3) + 1
-    digitGroup = [inputNum[-3:]]
-    for i in range(1, numOfDigitGroup - 1):
-        digitGroup.append(inputNum[-(3 * i + 3):-3 * i])
+    numOfDigitGroup = math.ceil(len(inputNum) / 3)
+    if numOfDigitGroup == 1:
+        digitGroup = list(inputNum)
+    else:
+        tempInputNum = inputNum
+        i = 1
+        digitGroup = []
+        while i <= numOfDigitGroup:
+            digitGroup.append(tempInputNum[-3:])
+            tempInputNum = tempInputNum[:-3]
+            i += 1
     return digitGroup
 
 
-def readSingleGroup(inputNum):
+def readSingleGroupFull(inputNum):
     readHundredDigit = listSingleDigit[int(inputNum[-3])] + " trăm"
 
     if inputNum[-2] == "1":
@@ -42,12 +50,42 @@ def readSingleGroup(inputNum):
     return readHundredDigit + readTenDigit + readUnitDigit
 
 
+def readSingleGroupMissing(inputNum):
+    if len(inputNum) == 1:
+        return listSingleDigit[int(inputNum)]
+    else:
+        if inputNum[-2] == "1":
+            readTenDigit = "mười"
+        else:
+            readTenDigit = listSingleDigit[int(inputNum[-2])] + " mươi"
+
+        if inputNum[-1] == "0":
+            readUnitDigit = ""
+        elif inputNum[-1] == "1":
+            if inputNum[-2] == "1":
+                readUnitDigit = " một"
+            else:
+                readUnitDigit = " mốt"
+        elif inputNum[-1] == "5":
+            readUnitDigit = " lăm"
+        else:
+            readUnitDigit = " " + listSingleDigit[int(inputNum[-1])]
+
+        return readTenDigit + readUnitDigit
+
+
 def numberToText(inputNum):
-    numberToText = ""
+    listGroupToText = []
     for i in range(len(splitDigitGroup(inputNum)) - 1, -1, -1):
-        numberToText += (readSingleGroup(splitDigitGroup(inputNum)
-                                               [i]) + " " + listDigitGroup[i])
-    return numberToText
+        if len(splitDigitGroup(inputNum)[i]) < 3:
+            listGroupToText.append(readSingleGroupMissing(
+                splitDigitGroup(inputNum)[i]))
+            listGroupToText.append(listDigitGroup[i])
+        else:
+            listGroupToText.append(readSingleGroupFull(
+                splitDigitGroup(inputNum)[i]))
+            listGroupToText.append(listDigitGroup[i])
+    return " ".join(listGroupToText)
 
 
 n = input("Nhập số : ")
